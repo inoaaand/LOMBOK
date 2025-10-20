03069316c6c943bdd9d174a64e176fa2f4bdac85
fer <20233005420@estudantes.ifpr.edu.br>
2025-10-20 07:47:16 -0300

    implementações DriverController

--- cars-main/src/main/java/br/edu/ifpr/cars/api/DriverController.java
+++ cars-main/src/main/java/br/edu/ifpr/cars/api/DriverController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import br.edu.ifpr.cars.domain.Driver;
 import br.edu.ifpr.cars.domain.DriverRepository;
@@ -13,12 +14,17 @@ import br.edu.ifpr.cars.domain.DriverRepository;
 @RestController
 public class DriverController {
 
-    @Autowired //INJEÇÃO DE DEPENDÊNCIAS 
-    DriverRepository driverRepository; //objeto instancia do repositório!
+    @Autowired // INJEÇÃO DE DEPENDÊNCIAS
+    DriverRepository driverRepository; // objeto instancia do repositório!
 
     @GetMapping("/drivers")
-    public List<Driver> listDrivers(){
+    public List<Driver> listDrivers() {
         return driverRepository.findAll();
+    }
+
+    @GetMapping("/drivers/{id}")
+    public Driver findDrivers(@PathVariable("id") Long id){
+        return driverRepository.findById(id).get(); //findById pega um driver optional e usamos o get p pegar do tipo driver
     }
 
 }
