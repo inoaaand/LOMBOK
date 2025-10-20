00905d0beb25f7eeae19064b4d5f13a55ca154d4
fer <20233005420@estudantes.ifpr.edu.br>
2025-10-20 08:14:26 -0300

    updates DriverController

--- cars-main/src/main/java/br/edu/ifpr/cars/api/DriverController.java
+++ cars-main/src/main/java/br/edu/ifpr/cars/api/DriverController.java
@@ -3,10 +3,17 @@ package br.edu.ifpr.cars.api;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
 import br.edu.ifpr.cars.domain.Driver;
 import br.edu.ifpr.cars.domain.DriverRepository;
 
@@ -22,9 +29,28 @@ public class DriverController {
         return driverRepository.findAll();
     }
 
+    // definir uma Exception personalizada!
     @GetMapping("/drivers/{id}")
-    public Driver findDrivers(@PathVariable("id") Long id){
-        return driverRepository.findById(id).get(); //findById pega um driver optional e usamos o get p pegar do tipo driver
+    public Driver findDrivers(@PathVariable("id") Long id) {
+        return driverRepository.findById(id).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+    }
+
+    @PostMapping("/drivers")
+    public Driver createDriver(@RequestBody Driver driver) {
+        return driverRepository.save(driver);
+    }
+
+    // update
+    @PutMapping("/drivers{id}")
+    public Driver fullUpdateDriver(@PathVariable("id") Long id, @RequestBody Driver driver) {
+        Driver foundDriver = findDrivers(id);
+        foundDriver.setName(driver.getName());
+        foundDriver.setBirthDate(driver.getBirthDate());
+
+        return driverRepository.save(foundDriver);
+
     }
 
 }
--- cars-main/src/main/java/br/edu/ifpr/cars/domain/Driver.java
+++ cars-main/src/main/java/br/edu/ifpr/cars/domain/Driver.java
@@ -3,13 +3,20 @@ package br.edu.ifpr.cars.domain;
 import java.time.LocalDate;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Entity
 @Data
 public class Driver {
+
+    @Id
+    @GeneratedValue 
     Long id;
+    
     String name;
+    
     LocalDate birthDate;
 
 }
