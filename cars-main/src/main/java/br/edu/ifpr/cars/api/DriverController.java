a2749dcb0a5eed87fbae2efcc789c854518f44aa
Taynara <taynaradasilva2001@gmail.com>
2025-10-21 14:04:25 -0300

    update DriverController

--- cars-main/src/main/java/br/edu/ifpr/cars/api/DriverController.java
+++ cars-main/src/main/java/br/edu/ifpr/cars/api/DriverController.java
@@ -1,16 +1,17 @@
 package br.edu.ifpr.cars.api;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -48,7 +49,16 @@ public class DriverController {
         Driver foundDriver = findDrivers(id);
         foundDriver.setName(driver.getName());
         foundDriver.setBirthDate(driver.getBirthDate());
+        return driverRepository.save(foundDriver);
 
+    }
+
+    @PatchMapping("/drivers/{id}")
+    public Driver incrementalUpdaDriver(@PathVariable("id") Long id, @RequestBody Driver driver) {
+        Driver foundDriver = findDrivers(id);
+
+        foundDriver.setName(Optional.ofNullable(driver.getName()).orElse(foundDriver.getName()));
+        foundDriver.setBirthDate(Optional.ofNullable(driver.getBirthDate()).orElse(foundDriver.getBirthDate()));
         return driverRepository.save(foundDriver);
 
     }
